25c6d960ec00a729c1a11fc9609154024c9fcf80
Mateo <monorymateo1@gmail.com>
2025-03-04 11:51:15 -0500

    cambio final

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function descargarContacto() {
     const vcardData = `BEGIN:VCARD
 VERSION:3.0
-N:Jimenez Arana;Javier;;;
-FN:Javier Jimenez Arana
-TEL:+57 315 5843205
-EMAIL;TYPE=WORK: gerencia@cervalle.com
-ADR;TYPE=WORK:;;Cra. 29b #11-90;Acopi;Yumbo;;Colombia
+N:Isaza León;Gonzalo;;;
+FN:Gonzalo Isaza León
+TEL:+57 314716018
+EMAIL;TYPE=WORK: directorpdv@cervalle.com
 END:VCARD`;
 
     const blob = new Blob([vcardData], { type: 'text/vcard' });
